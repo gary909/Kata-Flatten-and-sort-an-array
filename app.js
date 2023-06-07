c7ae7eb945b051bcd5ba79fc5df58d6a26c89306
gary909 <garywhite61@hotmail.com>
2023-06-07 12:39:13 +0200

    code working

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 function flattenAndSort(array) {
-    // Good luck, brave code warrior!
-    return array;
+    const mergeArr = array.flat(1); // Combine all arrs into one
+    return mergeArr.sort((a,b)=>a-b); // return sorted
 }
 
 console.log(flattenAndSort([])); //  []
 console.log(flattenAndSort([[], []])); //  []
-console.log(flattenAndSort([[], [1]])); //  [1])
+console.log(flattenAndSort([[], [1]])); //  [1]
 console.log(flattenAndSort([[3, 2, 1], [7, 9, 8], [6, 4, 5]])); //  [1, 2, 3, 4, 5, 6, 7, 8, 9]
 console.log(flattenAndSort([[1, 3, 5], [100], [2, 4, 6]])); //  [1, 2, 3, 4, 5, 6, 100]
